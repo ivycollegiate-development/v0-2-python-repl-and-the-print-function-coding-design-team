0f0170807011cf1a67949946eb31fffda4e977d1
Ethan6775 <Eyin26_student@ivycollegiate.org>
2025-10-16 09:07:17 +0000

    Second commit

---
+++ Ethan.py
@@ -0,0 +1,15 @@
+#This is a comment
+
+'''
+This is a multiple line
+comment using triple single quotes
+'''
+
+"""
+This is another multiple line
+comment. This time using triple
+double quotes
+"""
+
+
+print("Hello, world!")
