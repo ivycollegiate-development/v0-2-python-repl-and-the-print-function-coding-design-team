51a794aae99d5d84dece118f82f5647045b9bc1e
Paul Jones <paul.tyler.jones@gmail.com>
2025-10-16 09:06:16 +0000

    Second commit

---
+++ Jones.py
@@ -0,0 +1,16 @@
+# This is a comment
+
+'''
+This is a multiple line
+comment using triple single quotes
+'''
+
+"""
+This is another multiple line
+comment. This time using triple
+double quotes 
+"""
+
+#This is a multiple line
+#comment using the hash symbol
+print("Hello, world!")
