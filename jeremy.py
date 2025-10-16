3799589020ea934f2683c3bbfc9943ac35967318
Jeremy00002 <jeremy99051225@gmail.com>
2025-10-16 09:07:49 +0000

    second commit

---
+++ jeremy.py
@@ -0,0 +1,16 @@
+this is a commemt
+
+,,,
+this is a multiple linge
+commemt using thiple single quotes
+,,,
+
+,,,
+this is another mltiple line
+comment. this thime using triple
+double quotes
+,,,
+
+this is a mutiple line 
+#comment using the hash symbol
+print("hollo,world!")
