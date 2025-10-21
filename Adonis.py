63f8eba57404046902341257dd2f180a375ca610
ahuang31student-alt <ahuang31_student@ivycollegiate.org>
2025-10-21 09:07:48 +0000

    frist commit

---
+++ Adonis.py
@@ -0,0 +1,14 @@
+# this is a comment
+'''
+this is a multiple
+line comment
+'''
+"""
+This is another multiple line
+comment.
+"""
+#This is a multiple line
+#comment using the hash symbol
+
+print("Hello, world!")
+
