103e78a18d86951f9a811d5df33aa998d76b308e
KanoChiu <kchiu31_student@ivycollegiate.org>
2025-10-21 09:05:14 +0000

    First commit

---
+++ KAno.py
@@ -0,0 +1,13 @@
+
+''' 
+This is a mutiple
+line comment
+'''
+"""
+This is a another multiple line
+comment.
+"""
+
+
+
+print("Hello,world!")
---
+++ Kano.py
@@ -0,0 +1,14 @@
+
+
+
+This is a mutiple line
+commet using triple single quotes
+'''
+
+"""
+
+This is another mutiple line comment.This time using triple
+double quotes
+"""
+
+print("Hello World")
