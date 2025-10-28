acbf355888c5ec9f57f8c84c5d960a6fe2f982ad
ahuang31student-alt <ahuang31_student@ivycollegiate.org>
2025-10-28 09:18:11 +0000

    First commit

---
+++ python-Adonis.py
@@ -0,0 +1,6 @@
+print(15 + 15) # Addition
+print(15 - 14) # Subtraction
+print(15 * 2)  # Multiplication
+print(15 / 3)  # Divison
+print(15 // 3) # Mystery Operation
+print(15 % 3)  # Mystery Operation
